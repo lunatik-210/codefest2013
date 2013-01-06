ec12b1d57f9bcca8ecadf7cbec0f8410bcb08eee
Alexander Gudulin <alexandr.gudulin@gmail.com>
2013-01-06 04:58:48 +0400

    SplashScene was moved into a new class.

--- src/com/codefest2013/game/MainActivity.java
+++ src/com/codefest2013/game/MainActivity.java
@@ -1,14 +1,12 @@
 package com.codefest2013.game;
 
 import org.andengine.engine.camera.BoundCamera;
-import org.andengine.engine.camera.Camera;
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
-import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.svg.opengl.texture.atlas.bitmap.SVGBitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
@@ -19,7 +17,6 @@ import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtla
 import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
-import org.andengine.opengl.util.GLState;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.debug.Debug;
 
@@ -28,12 +25,11 @@ import android.view.WindowManager;
 
 public class MainActivity extends BaseGameActivity
 {
-    private Scene mSplashScene;
+    private SplashScene mSplashScene;
     private Scene mMainScene;
     
     private BitmapTextureAtlas mSplashTextureAtlas;
     private ITextureRegion mSplashTextureRegion;
-    private Sprite mSplash;
     
     private enum SceneType
     {
@@ -42,6 +38,7 @@ public class MainActivity extends BaseGameActivity
     }
     private SceneType mCurrentScene = SceneType.SPLASH;
     
+    //XXX: move these constants into resources class or something
     public static int CAMERA_WIDTH = 720;
     public static int CAMERA_HEIGHT = 480;
     public static int WORLD_WIDTH = 720;
@@ -77,6 +74,10 @@ public class MainActivity extends BaseGameActivity
         return engineOptions;
     }
 
+    /**
+     * Load resources for splash screen only
+     * Another resources are loading in loadResources() method
+     */
     @Override
     public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception
     {
@@ -88,10 +89,13 @@ public class MainActivity extends BaseGameActivity
         pOnCreateResourcesCallback.onCreateResourcesFinished();
     }
 
+    /**
+     * Call splash screen scene at first
+     */
     @Override
     public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception
     {
-        initSplashScene();
+        mSplashScene = new SplashScene(mEngine, mSplashTextureRegion, CAMERA_WIDTH, CAMERA_HEIGHT);
         pOnCreateSceneCallback.onCreateSceneFinished(mSplashScene);
     }
 
@@ -105,7 +109,7 @@ public class MainActivity extends BaseGameActivity
                 mEngine.unregisterUpdateHandler(pTimerHandler);
                 loadResources();
                 loadScenes();         
-                mSplash.detachSelf();
+                mSplashScene.detachSplash();
                 mEngine.setScene(mMainScene);
                 mCurrentScene = SceneType.MAIN;
             }
@@ -139,27 +143,6 @@ public class MainActivity extends BaseGameActivity
     private void loadScenes()
     {
         mMainScene = new GameScene();
-    }
-
-    /**
-     * Create splash screen
-     */
-    private void initSplashScene()
-    {
-        mSplashScene = new Scene();
-        mSplash = new Sprite(0, 0, mSplashTextureRegion, mEngine.getVertexBufferObjectManager())
-        {
-            @Override
-            protected void preDraw(GLState pGLState, Camera pCamera) 
-            {
-                super.preDraw(pGLState, pCamera);
-                pGLState.enableDither();
-            }
-        };
-        
-        mSplash.setScale(1.5f);
-        mSplash.setPosition((CAMERA_WIDTH - mSplash.getWidth()) * 0.5f, (CAMERA_HEIGHT - mSplash.getHeight()) * 0.5f);
-        mSplashScene.attachChild(mSplash);
     }
 
     public static MainActivity getSharedInstance() {
