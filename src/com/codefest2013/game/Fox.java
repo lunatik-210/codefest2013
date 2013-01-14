175b3f4d5c8d2fbccd1bf29d0092ab4e0e2c43d7
Denis Usanov <usanovdd@gmail.com>
2013-01-14 05:58:27 +0400

    Some code, next waypoint() function

--- src/com/codefest2013/game/Fox.java
+++ src/com/codefest2013/game/Fox.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Fox {
-	ArrayList<WayPoint> wps;
-	ArrayList<Integer> throwablePoints;
-	int current;
-	
-	public Fox(ArrayList<WayPoint> wayPointsArray, int startIndex) {
+	private ArrayList<WayPoint> wps;
+	private ArrayList<Integer> throwablePoints;
+	private int current;
+	private Random r;
+	public Fox(ArrayList<WayPoint> wayPointsArray, int startIndex){
+		/*
+		 * i don't know but 
+		 * 
+		if (wayPointsArray.size() < startIndex)
+		{
+			throw new Exception("Start index can't be more than count of points");
+		}
+		*/
+		r = new Random();
 		wps = wayPointsArray;
 		current = startIndex;
+		throwablePoints = new ArrayList<Integer>();
 		for(int i=0; i<wps.size(); i++)
 		{
 			if (wps.get(i).isThrowable)
@@ -17,13 +28,28 @@ public class Fox {
 			}
 		}
 	}
-	public Fox(ArrayList<WayPoint> wayPointsArray) {
+	public Fox(ArrayList<WayPoint> wayPointsArray){
 		this(wayPointsArray, 0);
 	}
 	
 	public int selectNext()
 	{
-		return 0;
+		/*
+		 * it wont work if we call not in throwable point	
+		int randomIndex = r.nextInt(throwablePoints.size());
+		int randomElement = throwablePoints.get(randomIndex);
+		throwablePoints.remove(randomIndex);
+		throwablePoints.add(current);
+		return randomElement;
+		//*/
+		
+		int randomIndex = r.nextInt(throwablePoints.size());
+		while (randomIndex == current)
+		{
+			randomIndex = r.nextInt(throwablePoints.size());
+		}
+		current = randomIndex; // no need in real life
+		return throwablePoints.get(randomIndex);
+		
 	}
-	
 }
--- src/com/codefest2013/game/WayPoint.java
+++ src/com/codefest2013/game/WayPoint.java
@@ -4,4 +4,11 @@ public class WayPoint {
 	public int y;
 	public int angle;
 	public boolean isThrowable;
+	public WayPoint(int x, int y, int angle, boolean isThrowable)
+	{
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+		this.isThrowable = isThrowable;
+	}
 }
