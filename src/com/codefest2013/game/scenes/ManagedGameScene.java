b9062cd0ad4f87bff35e59febcb65d22f88ffe3e
Andrew Lapin <andrew.d.lapin@gmail.com>
2013-02-05 12:13:48 +0400

    Refactored music value handler

--- src/com/codefest2013/game/scenes/ManagedGameScene.java
+++ src/com/codefest2013/game/scenes/ManagedGameScene.java
@@ -10,6 +10,7 @@ import com.codefest2013.game.scenes.objects.WayPoint;
 
 import org.andengine.audio.music.Music;
 import org.andengine.audio.music.MusicFactory;
+import org.andengine.entity.Entity;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
@@ -40,28 +41,6 @@ public class ManagedGameScene extends ManagedScene implements IOnSceneTouchListe
 	public boolean onSceneTouchEvent(Scene arg0, TouchEvent arg1) {
 		return mPlayer.onSceneTouchEvent(arg0, arg1);
 	}
-    
-	@Override
-	protected void onManagedUpdate(float pSecondsElapsed) {
-		float val = Math.abs(mBackground.getFirePlaceSprite().getSceneCenterCoordinates()[0]-mPlayer.getX());
-		float threshold = 1000.0f*mResourceManager.WORLD_SCALE_CONSTANT;
-		if(val > threshold) {
-			mResourceManager.fireplaceMusic.setVolume(0.0f);
-		}
-		else {
-			mResourceManager.fireplaceMusic.setVolume(1-(val/threshold));
-		}
-	
-		val = Math.abs(mBackground.getClockSprite().getSceneCenterCoordinates()[0]-mPlayer.getX());
-		threshold = 600.0f*mResourceManager.WORLD_SCALE_CONSTANT;
-		if(val > threshold) {
-			mResourceManager.tickTookMusic.setVolume(0.0f);
-		}
-		else {
-			mResourceManager.tickTookMusic.setVolume(1-(val/threshold));
-		}
-		super.onManagedUpdate(pSecondsElapsed);
-	}
 
 	@Override
 	public Scene onLoadingScreenLoadAndShown() {
@@ -90,30 +69,30 @@ public class ManagedGameScene extends ManagedScene implements IOnSceneTouchListe
 		ResourceManager.loadResources();
 		
 		WayPoint wps[] = {
-				new WayPoint(112, 230, 0, 0, false),
-				new WayPoint(170, 260, 0, 0, false),
-				new WayPoint(238, 120, 0, 0, false),
-				new WayPoint(264, 77, 0, 0, true),
-				new WayPoint(400, 110, 0, 0, false),
-				new WayPoint(424, 154, 45, 0, false),
-				new WayPoint(444, 218, 60, 0, false),
-				new WayPoint(474, 256, 0, 0, true),
-				new WayPoint(512, 182, 0, 0, false),
-				new WayPoint(526, 126, 0, 0, false),
-				new WayPoint(548, 210, 0, 45, false),
-				new WayPoint(595, 286, 0, 0, true),
-				new WayPoint(632, 366, 60, 0, false),
-				new WayPoint(668, 372, 0, 0, false),
-				new WayPoint(726, 264, 0, 0, false),
-				new WayPoint(948, 264, 0, 0, true),
-				new WayPoint(980, 370, 60, 45, false),
-				new WayPoint(1031, 450, 0, 0, false),
-				new WayPoint(1078, 494, 0, 0, false),
-				new WayPoint(1288, 528, 60, 0, true),
-				new WayPoint(1376, 482, 0, 0, false),
-				new WayPoint(1430, 406, 0, 0, false),
-				new WayPoint(1582, 264, 0, 0, false),
-				new WayPoint(1726, 190, 0, 0, true),
+			new WayPoint(112, 230, 0, 0, false),
+			new WayPoint(170, 260, 0, 0, false),
+			new WayPoint(238, 120, 0, 0, false),
+			new WayPoint(264, 77, 0, 0, true),
+			new WayPoint(400, 110, 0, 0, false),
+			new WayPoint(424, 154, 45, 0, false),
+			new WayPoint(444, 218, 60, 0, false),
+			new WayPoint(474, 256, 0, 0, true),
+			new WayPoint(512, 182, 0, 0, false),
+			new WayPoint(526, 126, 0, 0, false),
+			new WayPoint(548, 210, 0, 45, false),
+			new WayPoint(595, 286, 0, 0, true),
+			new WayPoint(632, 366, 60, 0, false),
+			new WayPoint(668, 372, 0, 0, false),
+			new WayPoint(726, 264, 0, 0, false),
+			new WayPoint(948, 264, 0, 0, true),
+			new WayPoint(980, 370, 60, 45, false),
+			new WayPoint(1031, 450, 0, 0, false),
+			new WayPoint(1078, 494, 0, 0, false),
+			new WayPoint(1288, 528, 60, 0, true),
+			new WayPoint(1376, 482, 0, 0, false),
+			new WayPoint(1430, 406, 0, 0, false),
+			new WayPoint(1582, 264, 0, 0, false),
+			new WayPoint(1726, 190, 0, 0, true),
 		};
 		
     	mBackground = new Background();
@@ -152,8 +131,6 @@ public class ManagedGameScene extends ManagedScene implements IOnSceneTouchListe
 
 	@Override
 	public void onHideScene() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -166,5 +143,24 @@ public class ManagedGameScene extends ManagedScene implements IOnSceneTouchListe
 				thisManagedGameScene.clearTouchAreas();
 				thisManagedGameScene.clearUpdateHandlers();
 			}});
+	}
+	
+	@Override
+	protected void onManagedUpdate(float pSecondsElapsed) {
+		manageMusicValue(mBackground.getFirePlaceSprite(), mResourceManager.fireplaceMusic, 1000.0f);
+		manageMusicValue(mBackground.getClockSprite(), mResourceManager.tickTookMusic, 600.0f);
+		super.onManagedUpdate(pSecondsElapsed);
+	}
+	
+	private void manageMusicValue( final Entity object, Music music, final float distance )
+	{
+		final float val = Math.abs(object.getSceneCenterCoordinates()[0]-mPlayer.getX());
+		final float threshold = distance*mResourceManager.WORLD_SCALE_CONSTANT;
+		if(val > threshold) {
+			music.setVolume(0.0f);
+		}
+		else {
+			music.setVolume(1-(val/threshold));
+		}
 	}
 }
